5828fe5e45b5af2683ce3b2e0cba5d2cb52e2ec6
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2020-12-28 09:34:11 -0300

    Creation of project #1

---
+++ mock-consumer.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# coding: utf-8 ===========================================================================
+# mock-consumer: A program that reads logs from a queue and pretends to send to a server log 
+# @author Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
+# @license LGPL-2.1
+# ===========================================================================
+from fluent import sender
+import json
+import os
+import requests
+import socket
+import stomp
+import time
+
+# connection to ActiveMQ
+def getQueue():
+    print("Loading queue configuration...")
+    try:
+        with open("queue.config.json", "r") as f:
+            queue_config = json.loads(f.read())
+    except IOError:
+        queue_config = {"QUEUE_USERNAME" : "", "QUEUE_PASSWORD": "", "QUEUE_HOST": "", "QUEUE_PORT": ""}
+
+    print("ActiveMQ configuration loaded from JSON file...")
+
+    if  os.getenv('QUEUE_USERNAME') is not None:
+        queue_username = os.getenv('QUEUE_USERNAME')
+    else:
+        queue_username = queue_config['QUEUE_USERNAME']
+    print("queue_username defined...")
+
+    if  os.getenv('QUEUE_PASSWORD') is not None:
+        queue_password = os.getenv('QUEUE_PASSWORD')
+    else:
+        queue_password = queue_config['QUEUE_PASSWORD']
+    print("queue_password defined...")
+
+    if  os.getenv('QUEUE_HOST') is not None:
+        queue_host = os.getenv('QUEUE_HOST')
+    else:
+        queue_host = queue_config['QUEUE_HOST']    
+    print("queue_host defined...")
+
+    if  os.getenv('QUEUE_PORT') is not None:
+        queue_port = int(os.getenv('QUEUE_PORT'))
+    else:
+        queue_port = int(queue_config['QUEUE_PORT'])
+    print("queue_port defined...")
+
+    print("Trying to connect with queue using " + queue_host + ":" + str(queue_port) + "...")
+    queue = stomp.Connection([(queue_host,queue_port)])
+    queue.connect(queue_username, queue_password, wait=True)
+    print("Connected to queue using user " + queue_username + " in host " + queue_host + ":" + str(queue_port))
+    queue.set_listener('logger',     LoggerListener())
+    queue.subscribe('/queue/pods',1)
+    print("Queue /queue/pods subscribed")
+    return queue  
+  
+class LoggerListener(stomp.ConnectionListener):
+    def on_error(self, frame, args):
+        if hasattr(frame, 'body'):
+            print('ERROR: received an error "%s"' % frame.body)
+        else:
+            print('ERROR: when tried to read messages')
+    def on_message(self, frame, args):
+        podips_host = getPodipsHost();
+        if hasattr(frame,'body'):
+            message = frame.body
+        else:
+            message = args
+        print('PODIPS-WRITER: received a message "%s"' % message)
+        try:
+            log = json.loads(message)
+            print("PODIPS-WRITER: message loaded as JSON")
+        except Exception as e:
+            print("ERROR: could not send log...:",e);
+
+# # # # # 
+# # # #
+# # # Main function
+# # 
+# 
+
+print("Waiting 10 seconds to start...")
+time.sleep(10)
+
+while True:
+    try:
+        queue = getQueue();
+        print("Waiting for messages...")
+        time.sleep(60)
+        queue.disconnect();
+    except Exception as e:
+        print("ERROR:",e)
+
+print("Program finished unexpectedly... and unexplainedly")
